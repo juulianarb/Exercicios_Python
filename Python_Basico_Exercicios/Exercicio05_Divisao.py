c3f2489c7e29d020b47594585026697b56bcbca1
Juliana Rodrigues <127902970+juulianarb@users.noreply.github.com>
2024-05-04 12:55:35 -0300

    Add files via upload

---
+++ Python_Basico_Exercicios/Exercicio05_Divisao.py
@@ -0,0 +1,14 @@
+# Curso básico de python
+# Nome do desenvolvedor: Juliana Rodrigues Brito
+# Versão 1.0
+# Exercício de Lógica de programação com a linguagem python
+
+# Programa que calcula a divisão de dois valores 
+
+print("Programa de divisão")
+
+primeiroNumero =float(input("Digite um número para calcular a divisão:"))
+segundoNumero = float(input("Digite um número para calcular a divisão:"))
+
+resultado = primeiroNumero / segundoNumero
+print("O resultado da soma dos números informados é de:" , resultado )
---
+++ Python_Basico_Exercicios/Exercicio13_Hexagono.py
@@ -0,0 +1,13 @@
+# Curso básico de python
+# Nome do desenvolvedor: Juliana Rodrigues Brito
+# Versão 1.0
+# Exercício de Lógica de programação com a linguagem python
+
+# Exercício que calcula a área de um hexagono
+
+base = float(input("Digite o valor da base do hexágono, para calcular a área:"))
+altura = float(input("Digite o valor da altura do hexágono, para calcular a área:"))
+
+area = 6 * base * altura
+
+print("A área do hexágono é de:" , area , " mt²")
